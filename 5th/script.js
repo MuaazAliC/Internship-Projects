bc5d990f407e6974510fdf0bd399b2a5788c66cb
MUHAMMAD MUAAZ ALI <154404670+MuaazAliC@users.noreply.github.com>
2025-07-16 11:45:22 +0500

    Update script.js

--- 5th/script.js
+++ 5th/script.js
@@ -25,3 +25,84 @@ document.addEventListener('click', (e) => {
     sidebar.classList.remove('active');
   }
 });
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("#sidebar .menu a");
+const btn = document.getElementById("btn_def"); 
+
+let clicked = false; 
+
+
+function hideAllSections() {
+  sections.forEach(section => {
+    section.style.display = "none";
+
+  });
+}
+
+
+function showSection(id) {
+  const target = document.getElementById(id);
+  if (target) {
+    target.style.display = "block";
+    btn.style.display="block"
+  }
+}
+
+
+navLinks.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    clicked = true; 
+
+    const sectionId = link.getAttribute("href").substring(1);
+
+    
+    navLinks.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+
+    
+    hideAllSections();
+    showSection(sectionId);
+  });
+});
+
+
+window.addEventListener("scroll", () => {
+  if (!clicked) {
+    let current = "";
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === "#" + current) {
+        link.classList.add("active");
+      }
+    });
+  }
+});
+
+
+btn.addEventListener("click", () => {
+  clicked = false; 
+
+  
+  sections.forEach(section => {
+    section.style.display = "block";
+    btn.style.display="none";
+  });
+
+  
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+  });
+});
