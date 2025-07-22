7639318e541562a01e02e0edee24d3a9a7fc8df9
MUHAMMAD MUAAZ ALI <154404670+MuaazAliC@users.noreply.github.com>
2025-07-22 11:54:37 +0500

    Create script.js

---
+++ 7th swiper js practice/script.js
@@ -0,0 +1,57 @@
+const swiper = new Swiper('.swiper', {
+  
+  
+  loop: true,
+
+  
+
+
+
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+     
+
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+   slidesPerView: 1,
+  spaceBetween: 10,
+  
+  breakpoints: {
+    
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    
+    640: {
+      slidesPerView: 5,
+      spaceBetween: 30
+    }
+  },
+   autoplay: {
+   delay: 1000,
+ },
+});
+
